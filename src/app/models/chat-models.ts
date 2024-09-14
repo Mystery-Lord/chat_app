30da76beb69c8e5d0d9c9ef33585086d74f997ee
Zecheng Pan <lebabehappy@gmail.com>
2024-09-14 11:25:06 +1000

    create user chat data model

---
+++ src/app/models/chat-models.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    users: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "users",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    admin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "messages",
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    groupName: {
+      type: String,
+      default: "",
+    },
+    groupDescription: {
+      type: String,
+      default: "",
+    },
+    groupProfileImage: {
+      type: String,
+      default: "",
+    },
+  },
+  {timestamps: true}
+);
+
+if (mongoose.models && mongoose.models["chats"]) {
+  mongoose.deleteModel("chats");
+}
+
+const chatModel = mongoose.model("chats", chatSchema);
+
+export default chatModel;
