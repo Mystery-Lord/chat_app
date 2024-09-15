8b485a26ca62d50f188a6ef16f5a032558931388
Zecheng Pan <lebabehappy@gmail.com>
2024-09-15 22:14:07 +1000

    start chats list and chat area design

---
+++ src/app/(private)/chat-components/chats-list/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function contactList() {
+  return (
+    <div className='w-[400px] h-full'>
+      contactList
+    </div>
+  )
+}
---
+++ src/app/(private)/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className=' flex h-[85vh]'>
+      
+    </div>
+  )
+}
